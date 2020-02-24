e9e5d4cf606708337f4a357071735da9bbb8d545
Piotr Kraszewski <kraszewskipiotr1@gmail.com>
2020-02-24 22:10:41 +0100

    deleted commented out stuff in js file

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,19 +1,6 @@
 // Check of specific todo by clicking
 $('ul').on("click", "li", function () { 
     $(this).toggleClass("completed");
-
-
-    // if($(this).css("color") === "rgb(128, 128, 128)"){
-    //     $(this).css({
-    //         color: "white",
-    //         textDecoration: "none"
-    //     });
-    // } else {
-    //     $(this).css({
-    //         color: "gray",
-    //         textDecoration: "line-through"
-    //     });
-    // }
 });
 
 
@@ -21,7 +8,7 @@ $("ul").on("click", "span", function (e) {
     $(this).parent().fadeOut(500, function(){   // this odnosi sie do span
         $(this).remove()                        // this odnosi sie do parent span czyli do 'li'
     })
-    e.stopPropagation();                        // zatrzymuje klikniecian a większych obiektach w któryh span się znajduje
+    e.stopPropagation();                        // zatrzymuje klikniecian a wiekszych obiektach w któryh span sie znajduje
 });
 
 
